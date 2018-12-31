c2924d32b64e80a3f617cf6b7241c775e934e111
muku787 <mukupoke@gmail.com>
2018-12-31 15:43:54 +0900

    当たり判定力学修正

--- gun/src/gun/events/DataPacketReceiveEvent.php
+++ gun/src/gun/events/DataPacketReceiveEvent.php
@@ -37,27 +37,28 @@ class DataPacketReceiveEvent extends Events {
 		        		return false;
 		        	}
 		        	$p->ticks['touch'] = $time + round(0.25, 5);
-		        	if($pk->transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY){
+		        	if(!isset($p->gun) or !gameManager::getTeam($p->getName())){}else{
+					if($p->isSneaking() and ($gun = $p->gun) !== null and !$p->reloading) {
+						$p->reloading = true;
+						$this->reload($p, $gun, 0);
+						break;
+					}
+					if($p->shot){
+						$p->shot = false;
+						break;
+					}
+					if(!$p->reloading and ($gun = $p->gun) !== null and $p->getInventory()->getItemInHand()->getId() !== 261){
+						$p->shot = true;
+						$this->beam->shot($p, $gun);
+						break;
+					}
+				}
+				if($pk->transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY){
 		        		if(is_null($pk->trData->entityRuntimeId) or $pk->trData->entityRuntimeId !== 114514) return false;
 		        		formManager::touch($pk, $p);
 		        		break;
 		        	}
-		        	if(!isset($p->gun) or !gameManager::getTeam($p->getName())) break;
-				if($p->isSneaking() and ($gun = $p->gun) !== null and !$p->reloading) {
-					$p->reloading = true;
-					$this->reload($p, $gun, 0);
-					break;
-				}
-				if($p->shot){
-					$p->shot = false;
-					break;
-				}
-				if(!$p->reloading and ($gun = $p->gun) !== null and $p->getInventory()->getItemInHand()->getId() !== 261){
-					$p->shot = true;
-					$this->beam->shot($p, $gun);
-					break;
-				}
-			break;
+				break;
 			case Info::MODAL_FORM_RESPONSE_PACKET:
 				formManager::receive($pk, $p);
 			break;	
--- gun/src/gun/weapons/beam.php
+++ gun/src/gun/weapons/beam.php
@@ -65,9 +65,10 @@ class beam {
                     			$y += $motionY;
                     			$z += $motionZ;
 					$pos = new Vector3($x, $y, $z);
+					$pos2 = new Vector3($x,$y+1,$z);
                     			foreach ($lv->getPlayers() as $player){
                     				if($player !== $p){
-                        				if($pos->distance($player) < 1){
+                        				if(abs($player->x - $x) <= $player->width + $rad && abs($player->z - $z) <= $player->width + $rad && abs(($player->y + $player->height * 0.5) - $y) <= $player->height + $rad){
                             					$headshot = false;
                             					if($player->getY() + 1.42 <= $y){
                                 					$headshot = true;
