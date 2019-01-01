e8523c595625962f6d9bc6965d983fe670a60826
kurarin_jp <fuji.ktbl@gmail.com>
2019-01-01 21:41:15 +0900

    bossbarフォルダ下を部品化したい

--- gun/src/gun/bossbar/BossBar.php
+++ gun/src/gun/bossbar/BossBar.php
@@ -4,18 +4,17 @@ namespace gun\bossbar;
 
 use pocketmine\Player;
 use pocketmine\Server;
-
 use pocketmine\math\Vector3;
-
+use pocketmine\event\Listener;
 use pocketmine\entity\Entity;
-
 use pocketmine\network\mcpe\protocol\AddEntityPacket;
 use pocketmine\network\mcpe\protocol\BossEventPacket;
 use pocketmine\network\mcpe\protocol\RemoveEntityPacket;
+//use pocketmine\network\mcpe\protocol\MoveEntityAbsolutePacket;
 use pocketmine\network\mcpe\protocol\SetEntityDataPacket;
 use pocketmine\network\mcpe\protocol\UpdateAttributesPacket;
 
-class BossBar{
+class BossBar implements Listener{
 
 	/*Mainクラスのオブジェクト*/
 	private $plugin;
@@ -32,10 +31,16 @@ class BossBar{
 	{
 		$this->plugin = $plugin;
 		$this->eid = Entity::$entityCount++;
+
+		//$this->BossBarTask();
+
+		$this->plugin->getScheduler()->scheduleRepeatingTask(new BossBarTask($this), 20);
+		$this->plugin->getServer()->getPluginManager()->registerEvents($this, $this->plugin);
 	}
 
 	public function show()
 	{
+		$this->visible = true;
 		foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
 			$this->showBossBar($player);
 		}
@@ -43,9 +48,15 @@ class BossBar{
 
 	public function hide()
 	{
+		$this->visible = false;
 		foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
 			$this->hideBossBar($player);
 		}
+	}
+
+	public function isVisivle()
+	{
+		return $this->visible;
 	}
 
 	public function setTitle($title)
@@ -152,4 +163,22 @@ class BossBar{
 		$bpk->playerEid = 0;
 		$player->dataPacket($bpk);
 	}
+
+	/*public function BossBarTask()
+	{
+		if($this->visible)
+		{
+			$mpk = new MoveEntityAbsolutePacket();
+			$mpk->flags |= MoveEntityAbsolutePacket::FLAG_TELEPORT;
+			$mpk->xRot = 0;
+			$mpk->yRot = 0;
+			$mpk->zRot = 0;
+			foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+				$mpk->position = $player->getPosition();
+				$player->dataPacket($mpk);
+			}
+		}
+		$this->plugin->getScheduler()->scheduleDelayedTask(new Callback([$this, 'BossBarTask'], []), 20);
+	}*/
+
 }
