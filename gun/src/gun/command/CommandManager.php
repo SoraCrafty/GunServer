1f07dfbf4249c2865ec48676dd8f37794e8a0f23
kurarin_jp <fuji.ktbl@gmail.com>
2019-02-17 16:50:35 +0900

    コマンドの追加忘れ

--- gun/src/gun/command/CommandManager.php
+++ gun/src/gun/command/CommandManager.php
@@ -9,6 +9,7 @@ use gun\command\commands\WeaponCommand;
 use gun\command\commands\WeaponShopCommand;
 use gun\command\commands\GuideBookCommand;
 use gun\command\commands\GameCommand;
+use gun\command\commands\DiscordCommand;
 
 class CommandManager{
 
@@ -20,6 +21,7 @@ class CommandManager{
         $map->register("battlefront", new WeaponShopCommand($plugin));
         $map->register("battlefront", new GuideBookCommand($plugin));
         $map->register("battlefront", new GameCommand($plugin));
+        $map->register("battlefront", new DiscordCommand($plugin));
     }
 
 }
