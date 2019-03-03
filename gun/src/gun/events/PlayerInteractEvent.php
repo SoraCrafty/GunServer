4d8cc205cf917ee96058f3c8f864c3d393b21258
SoraCrafty <potekosora224@gmail.com>
2019-03-02 23:10:13 -0300

    金リンゴの仕様を変

--- gun/src/gun/events/PlayerInteractEvent.php
+++ gun/src/gun/events/PlayerInteractEvent.php
@@ -2,10 +2,21 @@
 namespace gun\events;
 
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket; 
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+use pocketmine\item\Item;
 
 class PlayerInteractEvent extends Events {
 
 	public function call($event){
+		$player = $event->getPlayer();
+		$id = $player->getInventory()->getItemInHand()->getId();
+		$block = $event->getBlock()->getID();
+
+		if($id == 322 && $block == 0){
+			$player->getInventory()->removeItem(Item::get(322, 0, 1));
+			$player->addEffect(new EffectInstance(Effect::getEffect(10), 20 * 3, 3, false));
+		}
 		
 	}
 }
