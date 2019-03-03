899ccffa63f86d1481d7292788ae26d85e544246
kurarin_jp <fuji.ktbl@gmail.com>
2019-03-03 22:50:33 +0900

    星が二個つくのを直した

--- gun/src/gun/player/PlayerManager.php
+++ gun/src/gun/player/PlayerManager.php
@@ -90,7 +90,7 @@ class PlayerManager
 
 	public function setDefaultNameTags(Player $player)
 	{
-    	$tag = $player->getNameTag();
+    	$tag = $player->getName();
     	if($player->isOp()) $tag = "§b★§f{$tag}";
     	$player->setNameTag($tag);
     	$player->setDisplayName($tag);
